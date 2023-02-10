6d00be76e2d05f0222409b7c12ed696fb0dabe9f
Prinz Preince <prinzpreince@live.com>
2023-02-10 22:48:21 +0100

    js completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,33 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world";
+const myImage = document.querySelector("img");
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
 
-document.querySelector("html").addEventListener("click", function () {
-  alert("Ouch! Stop poking me!");
-});
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
